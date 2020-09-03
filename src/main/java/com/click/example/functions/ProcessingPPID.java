c520682e51e37611ebb346aaab7eee6fc49b1e65
Vivek Naskar <viveknaskar2912@gmail.com>
2020-09-03 18:51:18 +0530

    Fixed sonar issues

--- src/main/java/com/click/example/functions/ProcessingPPID.java
+++ src/main/java/com/click/example/functions/ProcessingPPID.java
@@ -1,17 +1,13 @@
 package com.click.example.functions;
 
-import com.click.example.StarterPipeline;
 import com.click.example.constants.KeyPrefix;
 import com.click.example.constants.RedisFieldIndex;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.values.KV;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.click.example.constants.PipelineConstants.REDIS_KEY_SEPARATOR;
 
 public class ProcessingPPID extends DoFn<String[], KV<String, KV<String, String>>> {
-    private final Logger LOGGER = LoggerFactory.getLogger(StarterPipeline.class);
 
     @ProcessElement
     public void processElement(@Element String[] fields, OutputReceiver<KV<String, KV<String, String>>> out) {
@@ -20,7 +16,6 @@ public class ProcessingPPID extends DoFn<String[], KV<String, KV<String, String>
             out.output(KV.of(KeyPrefix.GUID.toString()
                             .concat(REDIS_KEY_SEPARATOR).concat(fields[RedisFieldIndex.GUID.getValue()]),
                     KV.of(KeyPrefix.PPID.toString(), fields[RedisFieldIndex.PPID.getValue()])));
-            LOGGER.info("Created the hash!");
         }
     }
 }
