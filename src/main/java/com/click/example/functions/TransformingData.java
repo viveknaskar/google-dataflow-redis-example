d354eb211b49d98f03c149ebef1a9ad750737417
Vivek Naskar <viveknaskar2912@gmail.com>
2020-09-06 00:24:27 +0530

    Added validation for guid, firstname,lastname, postcode  and dob

--- src/main/java/com/click/example/functions/TransformingData.java
+++ src/main/java/com/click/example/functions/TransformingData.java
@@ -1,5 +1,7 @@
 package com.click.example.functions;
 
+import com.click.example.constants.RedisFieldIndex;
+import com.google.api.client.util.Strings;
 import org.apache.beam.sdk.transforms.DoFn;
 
 import static com.click.example.constants.PipelineConstants.REGEX_LINE_SPLITTER_PIPE;
@@ -9,7 +11,19 @@ public class TransformingData extends DoFn<String, String[]> {
     @ProcessElement
     public void processElement(@Element String line, OutputReceiver<String[]> out) {
         String[] fields = line.split(REGEX_LINE_SPLITTER_PIPE);
-        out.output(fields);
+        /**
+         * Added validation for guid, firstname,lastname, postcode and dob as these are
+         * mandatory fields. So these fields are missing in the file uploaded in the input GCS bucket
+         * the complete line will be excluded.
+         * Sample input file with missing mandatory fields is added with this repo - validationcheckmandatory.txt
+         */
+        if (!Strings.isNullOrEmpty(fields[RedisFieldIndex.GUID.getValue()]) &&
+                !Strings.isNullOrEmpty(fields[RedisFieldIndex.FIRSTNAME.getValue()]) &&
+                !Strings.isNullOrEmpty(fields[RedisFieldIndex.LASTNAME.getValue()]) &&
+               !Strings.isNullOrEmpty(fields[RedisFieldIndex.DOB.getValue()]) &&
+                !Strings.isNullOrEmpty(fields[RedisFieldIndex.POSTAL_CODE.getValue()])) {
+            out.output(fields);
+        }
     }
 
 }
