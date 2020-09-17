1a022033d80df0deee0d111651edeea3d06bd58f
Vivek Naskar <viveknaskar2912@gmail.com>
2020-09-17 13:06:09 +0530

    Logging failed records if any when the input file is processed.

--- src/main/java/com/click/example/functions/TransformingData.java
+++ src/main/java/com/click/example/functions/TransformingData.java
@@ -3,14 +3,20 @@ package com.click.example.functions;
 import com.click.example.constants.RedisFieldIndex;
 import com.google.api.client.util.Strings;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.click.example.constants.PipelineConstants.REGEX_LINE_SPLITTER_PIPE;
 
 public class TransformingData extends DoFn<String, String[]> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransformingData.class);
+
     @ProcessElement
     public void processElement(@Element String line, OutputReceiver<String[]> out) {
         String[] fields = line.split(REGEX_LINE_SPLITTER_PIPE);
+        StringBuilder reason = new StringBuilder();
+
         /**
          * Added validation for guid, firstname,lastname, postcode and dob as these are
          * mandatory fields. So these fields are missing in the file uploaded in the input GCS bucket
@@ -23,8 +29,36 @@ public class TransformingData extends DoFn<String, String[]> {
                 !Strings.isNullOrEmpty(fields[RedisFieldIndex.LASTNAME.getValue()]) &&
                 !Strings.isNullOrEmpty(fields[RedisFieldIndex.DOB.getValue()]) &&
                 !Strings.isNullOrEmpty(fields[RedisFieldIndex.POSTAL_CODE.getValue()])) {
+
             out.output(fields);
+
+        } else {
+
+            LOGGER.info("Validation failed for GUID: " + fields[RedisFieldIndex.GUID.getValue()] + " and the line is: " + line);
+
+            if (Strings.isNullOrEmpty(fields[RedisFieldIndex.GUID.getValue()])){
+                reason.append("Validation failed due to GUID empty or null" );
+            }
+            else if (Strings.isNullOrEmpty(fields[RedisFieldIndex.PPID.getValue()])) {
+                reason.append("Validation failed due to PPID empty or null" );
+            }
+            else if (Strings.isNullOrEmpty(fields[RedisFieldIndex.FIRSTNAME.getValue()])) {
+                reason.append("Validation failed due to First Name empty or null" );
+            }
+            else if (Strings.isNullOrEmpty(fields[RedisFieldIndex.LASTNAME.getValue()])) {
+                reason.append("Validation failed due to Last Name empty or null" );
+            }
+            else if (Strings.isNullOrEmpty(fields[RedisFieldIndex.DOB.getValue()])) {
+                reason.append("Validation failed due to DOB empty or null" );
+            } else {
+                reason.append("Validation failed due to Postal Code empty or null" );
+            }
+
+            LOGGER.info(reason.toString());
+
         }
+
     }
+
 
 }
